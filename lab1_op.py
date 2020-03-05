1beff6a4ad215d2f795f3d664c6c3dbf3fb256ae
buhairomka <buhairomka@ukr.net>
2020-03-05 17:16:50 +0200

    виправив глупу ошибку

--- lab1_op.py
+++ lab1_op.py
@@ -4,7 +4,7 @@ matr=[[random.randint(0,10) for i in range(10)] for j in range(10)]
 for i in matr:
 	print(i)
 
-nextmatr=[]
+nextmatr=[[0 for i in range(10)] for j in range(10)]
 for i in range(len(matr)):
 	for j in range(len(matr[0])):
 		nextmatr[i][j]=matr[j][i]
