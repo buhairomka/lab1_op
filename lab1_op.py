9bc69fb3a0e2bf697f26976b58691bbe9ac44e7a
buhairomka <buhairomka@ukr.net>
2020-03-05 17:13:08 +0200

    я хз шо я зробив

--- lab1_op.py
+++ lab1_op.py
@@ -3,3 +3,11 @@ import random
 matr=[[random.randint(0,10) for i in range(10)] for j in range(10)]
 for i in matr:
 	print(i)
+
+nextmatr=[]
+for i in range(len(matr)):
+	for j in range(len(matr[0])):
+		nextmatr[i][j]=matr[j][i]
+for i in nextmatr:
+	print(i)
+		
