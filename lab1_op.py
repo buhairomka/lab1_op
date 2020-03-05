4192e0368f128d20d8bac4f54051ee64902a7b68
buhairomka <buhairomka@ukr.net>
2020-03-05 17:18:00 +0200

    Update lab1_op.py

--- lab1_op.py
+++ lab1_op.py
@@ -3,7 +3,7 @@ import random
 matr=[[random.randint(0,10) for i in range(10)] for j in range(10)]
 for i in matr:
 	print(i)
-
+print()
 nextmatr=[[0 for i in range(10)] for j in range(10)]
 for i in range(len(matr)):
 	for j in range(len(matr[0])):
